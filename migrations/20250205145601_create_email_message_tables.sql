6212a311f1cbd1f53f30376f81688239dac91a4b
Chuck Chen <chuck.j.chen@gmail.com>
2025-03-05 10:59:11 +0800

    Update schema for email and email optouts

--- migrations/20250205145601_create_email_message_tables.sql
+++ migrations/20250205145601_create_email_message_tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE "contacts" (
 -- CreateTable
 CREATE TABLE "projects" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "project_key" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
     "display_name" TEXT NOT NULL,
     "description" TEXT,
     "is_active" BOOLEAN NOT NULL DEFAULT true,
@@ -21,11 +21,11 @@ CREATE TABLE "projects" (
 CREATE TABLE "templates" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "project_id" INTEGER NOT NULL,
-    "template_key" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
     "display_name" TEXT NOT NULL,
     "description" TEXT,
     "subject" TEXT NOT NULL,
-    "body_html" TEXT,
+    "body_html" TEXT NOT NULL,
     "body_text" TEXT NOT NULL,
     "is_active" BOOLEAN NOT NULL DEFAULT true,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -37,35 +37,36 @@ CREATE TABLE "templates" (
 CREATE TABLE "messages" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "contact_id" INTEGER NOT NULL,
-    "template_id" INTEGER NOT NULL,
+    "template_id" INTEGER,
     "status" TEXT NOT NULL DEFAULT 'PENDING',
     "error_details" TEXT,
     "variables" TEXT,
+    "external_id" TEXT,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "sent_at" DATETIME,
     CONSTRAINT "messages_contact_id_fkey" FOREIGN KEY ("contact_id") REFERENCES "contacts" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "messages_template_id_fkey" FOREIGN KEY ("template_id") REFERENCES "templates" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "messages_template_id_fkey" FOREIGN KEY ("template_id") REFERENCES "templates" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 -- CreateTable
-CREATE TABLE "template_blocks" (
+CREATE TABLE "template_optouts" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "contact_id" INTEGER NOT NULL,
     "template_id" INTEGER NOT NULL,
-    "block_reason" TEXT,
+    "reason" TEXT,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "template_blocks_contact_id_fkey" FOREIGN KEY ("contact_id") REFERENCES "contacts" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "template_blocks_template_id_fkey" FOREIGN KEY ("template_id") REFERENCES "templates" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "template_optouts_contact_id_fkey" FOREIGN KEY ("contact_id") REFERENCES "contacts" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "template_optouts_template_id_fkey" FOREIGN KEY ("template_id") REFERENCES "templates" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "contacts_email_address_key" ON "contacts"("email_address");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "projects_project_key_key" ON "projects"("project_key");
+CREATE UNIQUE INDEX "projects_key_key" ON "projects"("key");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "templates_project_id_template_key_key" ON "templates"("project_id", "template_key");
+CREATE UNIQUE INDEX "templates_project_id_key_key" ON "templates"("project_id", "key");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "template_blocks_contact_id_template_id_key" ON "template_blocks"("contact_id", "template_id");
+CREATE UNIQUE INDEX "template_optouts_contact_id_template_id_key" ON "template_optouts"("contact_id", "template_id");
