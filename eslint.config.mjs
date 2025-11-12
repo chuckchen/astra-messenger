19ace5a75da050136561e8d24a1c014b0787dc15
Chuck Chen <chuck.j.chen@gmail.com>
2025-11-12 17:42:02 +0800

    Ignore wrangler directory for linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -35,6 +35,6 @@ export default [
 		},
 	},
 	{ files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
-	{ ignores: ['node_modules/', 'dist/', 'build/'] },
+	{ ignores: ['node_modules/', 'dist/', 'build/', '.wrangler/'] },
 	eslintPluginPrettierRecommended,
 ];
